712335586fd0b424b86053f327fcfa779616c037
Christoffer Pettersson <corgrath@gmail.com>
2015-12-02 10:11:19 +0100

    Update greasemonkey-ns-pdb.user.js

--- greasemonkey-ns-pdb.user.js
+++ greasemonkey-ns-pdb.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        greasemonkey-ns-pdb
 // @include     https://projekt.netset.se/*
-// @version     16
+// @version     17
 // @downloadURL https://github.com/corgrath/greasemonkey-ns-pdb/raw/master/greasemonkey-ns-pdb.user.js
 // @updateURL   https://github.com/corgrath/greasemonkey-ns-pdb/raw/master/greasemonkey-ns-pdb.user.js
 // @grant       none
